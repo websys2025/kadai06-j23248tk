9ea21f230f3bc0ce7c54f2d014d37e4d63f1b61c
j23248 髙橋慶太 <j23248tk@edu.tuis.ac.jp>
2025-05-26 11:08:28 +0900

    Update Kadai06-2.py

--- .github/Kadai06-2.py
+++ .github/Kadai06-2.py
@@ -1,9 +1,1 @@
-import requests
-import json
 
-url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
-
-response = requests.get(url)
-data = response.json()
-
-print(json.dumps(data, indent=2, ensure_ascii=False))
